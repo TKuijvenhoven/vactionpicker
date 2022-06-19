e4f73c223651c53aa65588de3e3a7a0096302383
Thomas Kuijvenhoven <302612533@student.rocmondriaan.nl>
2022-06-19 16:15:55 +0200

    mappenstructuur aangepast

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+<!--  <HelloWorld msg="This is my first vue project"/>-->
+<!--  <CustomerList idk="nieuwe tekst" />-->
+  <vacationPicker />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import CustomerList from "@/components/CustomerList";
+import vacationPicker from "@/components/vacationPicker";
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    // CustomerList,
+    vacationPicker
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
